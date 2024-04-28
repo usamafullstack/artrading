2fe86f20a823061515573597c313b66813ffe151
Reznov00 <cena.amz@gmail.com>
2024-04-28 21:38:02 +0500

    200

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
   const location = useLocation();
   const [dropdown, showDropdown] = useState(false);
   return (
-    <nav className="flex items-center justify-between px-10 mt-5 bg-primary relative">
+    <nav className="flex items-center justify-between px-10 mt-5 bg-primary">
       <img
         alt="Lint Fix"
         className="cursor-pointer"
@@ -19,48 +19,62 @@ const Navbar = () => {
         onClick={() => navigate("/")}
       />
       <div className="flex gap-20 ">
-        {ROUTING_OPTIONS.map((option) => {
-          return (
-            <li
-              key={option.route}
-              className={`text-xl hover:text-secondary cursor-pointer list-none ${
-                location.pathname === option.route
-                  ? "text-secondary"
-                  : "text-white"
-              } ${dropdown && "text-secondary"}`}
-              onMouseOver={() =>
-                option.name === "Products" && showDropdown(true)
-              }
-              onMouseOut={() =>
-                option.name === "Products" && showDropdown(false)
-              }
-              onClick={() =>
-                option.name !== "Products" && navigate(`${option.route}`)
-              }>
-              {option.name}
-            </li>
-          );
-        })}
-      </div>
-      {dropdown && (
-        <section
-          className="bg-white text-black h-32 w-48 absolute right-[435px] top-16 flex flex-col justify-evenly p-2 rounded-3xl"
+        <li
+          className={`text-xl hover:text-secondary cursor-pointer list-none ${
+            location.pathname === "/" ? "text-secondary" : "text-white"
+          } ${dropdown && "text-secondary"}`}
+          onClick={() => navigate("/")}>
+          Home
+        </li>
+        <li
+          className={`text-xl hover:text-secondary cursor-pointer list-none ${
+            location.pathname === "/about" ? "text-secondary" : "text-white"
+          } ${dropdown && "text-secondary"}`}
+          onClick={() => navigate("/about")}>
+          About
+        </li>
+        <li
+          className={`text-xl hover:text-secondary cursor-pointer list-none ${
+            location.pathname === "/services" ? "text-secondary" : "text-white"
+          } ${dropdown && "text-secondary"}`}
+          onClick={() => navigate("/services")}>
+          Services
+        </li>
+        <li
+          className={`text-xl relative hover:text-secondary cursor-pointer list-none ${
+            location.pathname === "/fooditems" ||
+            location.pathname === "/nonfooditems"
+              ? "text-secondary"
+              : "text-white"
+          } ${dropdown && "text-secondary"}`}
           onMouseOver={() => showDropdown(true)}
           onMouseOut={() => showDropdown(false)}>
-          <a
-            className="rounded-full hover:text-hover hover:font-bold w-full h-full text-center pt-5"
-            // href={`${window.location.protocol}//${window.location.host}/fooditems`}
-            onClick={() => navigate("fooditems", { replace: true })}>
-            Food Items
-          </a>
-          <a
-            className="rounded-full hover:text-hover hover:font-bold w-full h-full text-center pt-5"
-            // href={`${window.location.protocol}//${window.location.host}/nonfooditems`}
-            onClick={() => navigate("nonfooditems", { replace: true })}>
-            Non-Food Items
-          </a>
-        </section>
-      )}
+          Products
+          <section
+            className={`${
+              dropdown ? "block" : "hidden"
+            } bg-white text-black h-32 w-48 absolute top-7 -left-12 flex flex-col justify-evenly p-2 rounded-3xl`}
+            onMouseOver={() => showDropdown(true)}
+            onMouseOut={() => showDropdown(false)}>
+            <a
+              className="rounded-full hover:text-hover hover:font-bold w-full h-full text-center pt-5"
+              onClick={() => navigate("/fooditems", { replace: true })}>
+              Food Items
+            </a>
+            <a
+              className="rounded-full hover:text-hover hover:font-bold w-full h-full text-center pt-5"
+              onClick={() => navigate("/nonfooditems", { replace: true })}>
+              Non-Food Items
+            </a>
+          </section>
+        </li>
+        <li
+          className={`text-xl hover:text-secondary cursor-pointer list-none ${
+            location.pathname === "/contact" ? "text-secondary" : "text-white"
+          } ${dropdown && "text-secondary"}`}>
+          Contact
+        </li>
+      </div>
       <Button
         text={"Contact Us"}
         navigateTo={"contact"}
